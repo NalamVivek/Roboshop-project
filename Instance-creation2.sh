367eed220b6b4eb9c01a17c66d1ae6e1ca64b497
Vivek Vardhan <nalamvivekvardhan@gmail.com>
2025-10-05 15:23:53 +0530

    Completed preparing the script

--- Instance-creation2.sh
+++ Instance-creation2.sh
@@ -6,7 +6,8 @@
 
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-0ca74d0228955aa4a
-
+ZONE_ID=Z01001791U7GXL8V30X8G
+DOMAIN_NAME=speakaholics.fun
 
 for INSTANCE in $@
 do 
@@ -16,37 +17,31 @@ do
     #Get IP based on condition
     if [ $INSTANCE != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        #RECORD_NAME="$instance.$DOMAIN_NAME"
+        RECORD_NAME="$instance.$DOMAIN_NAME"
         else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        #RECORD_NAME="$DOMAIN_NAME"
+        RECORD_NAME="$DOMAIN_NAME"
     fi 
 
     echo "$INSTANCE: $IP"
 
+    #Updating route53 records
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": 
+        [{
+         "Action"               : "UPSERT"
+         ,"ResourceRecordSet"   : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+               "Value"         : "'$IP'"
+            }]
+         }
+        }]
+    }'
 done
-
-
-#=================================#
-## Updating route53 records ##
-#=================================#
-
-
-#    aws route53 change-resource-record-sets \
-#    --hosted-zone-id $ZONE_ID \
-#    --change-batch '
-#    {
-#        "Comment": "Updating record set"
-#        ,"Changes": [{
-#        "Action"              : "UPSERT"
-#        ,"ResourceRecordSet"  : {
-#            "Name"              : "'$RECORD_NAME'"
-#            ,"Type"             : "A"
-#            ,"TTL"              : 1
-#            ,"ResourceRecords"  : [{
-#               "Value"         : "'$IP'"
-#            }]
-#        }
-#        }]
-#    }
-#    '
